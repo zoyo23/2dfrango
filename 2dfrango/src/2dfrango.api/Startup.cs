65dbc6c6ffca253e1c6d27474d5ba02a62b4d6f1
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-05-01 11:32:04 -0300

    Adição do Swagger e alteraçao de versão do aspnet

--- 2dfrango/src/2dfrango.api/Startup.cs
+++ 2dfrango/src/2dfrango.api/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace _2dfrango.api
 {
@@ -26,11 +20,33 @@ namespace _2dfrango.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            #region Swagger Configuration
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "2DFrango API",
+                    Version = "v1",
+                    Description = "Api referente ao Projeto desenvonvido no Hackathon Mega Hack."
+                });
+            });
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            #region Swagger Configuration
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "2DFrango API v1");
+                c.RoutePrefix = string.Empty;
+            });
+            #endregion
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
