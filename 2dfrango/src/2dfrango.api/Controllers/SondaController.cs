c44e893cc8a006e2682643ce563d1921f8df3e18
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-05-01 17:51:54 -0300

    Criação do método Sonda

---
+++ 2dfrango/src/2dfrango.api/Controllers/SondaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace _2dfrango.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SondaController : ControllerBase
+    {
+        [HttpGet]
+        public string Sonda()
+        {
+            return new Random().Next(1000, 10000).ToString();
+        }
+    }
+}
