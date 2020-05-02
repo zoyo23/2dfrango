1dc975f9eb68730c6cfef227e1a7b04bcc365730
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-05-02 10:41:08 -0300

    Instalação de entity framework

--- 2dfrango/src/2dfrango.infra.repository/Repositories/AutenticacaoRepository.cs
+++ 2dfrango/src/2dfrango.infra.repository/Repositories/AutenticacaoRepository.cs
@@ -1,8 +1,20 @@
 ﻿using _2dfrango.domain.Interfaces.Repositories;
+using System.Data;
 
 namespace _2dfrango.infra.repository
 {
     public class AutenticacaoRepository : IAutenticacaoRepository
     {
+        #region Atributos
+        private readonly IDbConnection _dbConnection;
+
+        #endregion
+
+        #region Construtores
+        public AutenticacaoRepository(IDbConnection dbConnection)
+        {
+            _dbConnection = dbConnection;
+        }
+        #endregion
     }
 }
--- 2dfrango/src/2dfrango.service/Services/AutenticacaoService.cs
+++ 2dfrango/src/2dfrango.service/Services/AutenticacaoService.cs
@@ -1,8 +1,19 @@
-﻿using _2dfrango.domain.Interfaces.Services;
+﻿using _2dfrango.domain.Interfaces.Repositories;
+using _2dfrango.domain.Interfaces.Services;
 
 namespace _2dfrango.service.Services
 {
     public class AutenticacaoService : IAutenticacaoService
     {
+        #region Atributos
+        private readonly IAutenticacaoRepository _autenticacaoRepository;
+        #endregion
+
+        #region Construtores
+        public AutenticacaoService(IAutenticacaoRepository autenticacaoRepository)
+        {
+            _autenticacaoRepository = autenticacaoRepository;
+        }
+        #endregion
     }
 }
