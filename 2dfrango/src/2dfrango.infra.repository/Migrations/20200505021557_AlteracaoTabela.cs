796d0ac7f4ea6104c17cf368acc2574aeab37ffe
Zoyo <lucasoliveiraoueiroz@gmail.com>
2020-05-04 23:30:53 -0300

    Criação de migração

---
+++ 2dfrango/src/2dfrango.infra.repository/Migrations/20200505021557_AlteracaoTabela.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace _2dfrango.infra.repository.Migrations
+{
+    public partial class AlteracaoTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
